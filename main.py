a470612d4e2530c74e8fa0da07285a3ccc6e4f80
romanychev-l <leonid20.0831@gmail.com>
2020-10-29 08:42:13 +0000

    Rewrite on aiogram library

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import config
-import telebot
 import csv
 import random
 import time
+import asyncio
 
-bot = telebot.TeleBot(config.token)
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.executor import start_webhook
+
+
+WEBHOOK_HOST = 'https://romanychev.online'
+WEBHOOK_PATH = '/dictionary/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '127.0.0.1'
+WEBAPP_PORT = 7772
+
+bot = Bot(token=config.token)
+dp = Dispatcher(bot)
 
 dictionary = {}
 fieldnames = ['words', 'abbreviations', 'translate']
@@ -17,10 +29,11 @@ path_in = "dict_in.csv"
 
 def csv_reader(file_obj):
     reader = csv.reader(file_obj)
-    
+
     for row in reader:
         if(row[0] != 'words'):
             dictionary[row[0]] = row[2]
+
 
 def csv_dict_writer(data):
     with open(path_in, "w", newline='') as out_file:
@@ -28,6 +41,7 @@ def csv_dict_writer(data):
         writer.writeheader()
         for row in data:
             writer.writerow(row)
+
 
 def save():
     global dictionary
@@ -46,6 +60,7 @@ def save():
 
     csv_dict_writer(data)
 
+
 def parse_str(s):
     i = 0
     n = len(s)
@@ -61,40 +76,42 @@ def parse_str(s):
         t = s[l:i]
     return t
 
-@bot.message_handler(commands=["add"])
-def add_word(message):
+
+@dp.message_handler(commands=["add"])
+async def add_word(message):
     text = parse_str(message.text)
     dictionary[text[0]] = text[1]
-        
+
     save()
 
-    bot.send_message(message.chat.id, "Word is added")
+    await bot.send_message(message.chat.id, "Word is added")
 
-@bot.message_handler(commands=["get"])
-def get_word(message):
+@dp.message_handler(commands=["get"])
+async def get_word(message):
     save()
     print(len(words))
     index = random.randint(0, len(words) - 1)
-    bot.send_message(message.chat.id, words[index])
-    time.sleep(3)
-    bot.send_message(message.chat.id, translate[index])
+    await bot.send_message(message.chat.id, words[index])
+    await asyncio.sleep(3)
+    await bot.send_message(message.chat.id, translate[index])
 
-@bot.message_handler(commands=["delete"])
-def delete(message):
+@dp.message_handler(commands=["delete"])
+async def delete(message):
     text = parse_str(message.text)
     if(text in dictionary):
         del dictionary[text]
         save()
-        bot.send_message(message.chat.id, "Word is deleted")
+        await bot.send_message(message.chat.id, "Word is deleted")
     else:
-        bot.send_message(message.chat.id, "Word isn`t found")
+        await bot.send_message(message.chat.id, "Word isn`t found")
 
-@bot.message_handler(commands=["training"])
-def training(message):
+
+@dp.message_handler(commands=["training"])
+async def training(message):
     kol = 0
     n = len(dictionary)
     used = [0] * n
-   
+
     save()
     while(kol < n):
         index = random.randint(0, n)
@@ -102,13 +119,26 @@ def training(message):
             index = random.randint(0, n)
         used[index] = 1
         kol += 1
-        bot.send_message(message.chat.id, words[index])
-        time.sleep(3)
-        bot.send_message(message.chat.id, translate[index])
-        time.sleep(3)
+        await bot.send_message(message.chat.id, words[index])
+        await asyncio.sleep(3)
+        await bot.send_message(message.chat.id, translate[index])
+        await asyncio.sleep(3)
+
+
+def main():
+    with open(path_in, "r") as f_obj:
+        csv_reader(f_obj)
+
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
 
 
 if __name__ == '__main__':
-    with open(path_in, "r") as f_obj:
-        csv_reader(f_obj)
-    bot.polling(none_stop=True)
+    main()
